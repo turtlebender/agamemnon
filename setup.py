4cdff6c202a1a0a367a5d1b0934b2b3220fe0c05
Tom Howe <trhowe@ci.uchicago.edu>
2011-03-10 13:56:40 -0600

    adding manifest.in

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@ import os
 from setuptools import setup, find_packages
 
 here = os.path.abspath(os.path.dirname(__file__))
-README = open(os.path.join(here, 'README.rst')).read()
-CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
+README = open('README.rst').read()
+CHANGES = open('CHANGES.txt').read()
 
 requires = ['pycassa', 'thrift05', 'setuptools']
 tests_requires = requires + ['nose', 'mock']
 
 setup(name='agamemnon',
-      version='0.1.0',
+      version='0.1.1',
       description='A graph database built on top of cassandra',
       long_description=README + "\n\n" + CHANGES,
       classifiers=[
